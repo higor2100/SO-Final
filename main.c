0a7a51e6d74e2b63d40014dd519c3a049f8bc7ba
higor2100 <higor2100@gmail.com>
2020-11-19 20:19:34 -0300

    Final subir

--- main.c
+++ main.c
@@ -1,113 +1,112 @@
 #include<stdio.h>
 #include<stddef.h>
 
-//MAX memoria SIZE
+// TAMANHO MÁXIMO DA MEMÓRIA
 char memoria[20000];
 
-//GENERAL STRUCTURE FOR BLOCKS ALLOCATED AND DEALLOCATED
-struct block{
- size_t tamanho; //tamanho of the block
- int free; //flag for if block is free or not
- struct block *next; //pointer to the next block
+// ESTRUTURA GERAL PARA BLOCOS ALOCADOS E DESALOCADOS
+struct bloco{
+    size_t tamanho; // tamanho do bloco
+    int libera; // sinaliza se o bloco está livre ou não
+     struct bloco *prox; // ponteiro para o bloco prox
 };
 
 
-//initial block declaration
-struct block *freeList=(void*)memoria;
+// declaração de bloco inicial
+struct bloco *memoLivre=(void*)memoria;
 
-//HELPER FUNCTIONS - DEFINED LATER IN THE CODE
-void initialize();
-void split(struct block *fitting_slot,size_t tamanho);
-void *mymalloc(size_t noOfBytes);
-void merge();
-void myfree(void* ptr);
+// FUNÇÕES DO AJUDANTE - DEFINIDAS MAIS TARDE NO CÓDIGO
+void  inicializar();
+void quebra(struct bloco *encaixe_de_memo,size_t tamanho);
+void *aloca(size_t nDeBytes);
+void misturar();
+void libera(void* ptr);
 
-//initializing the block of memoria with max tamanho (freelist is the main block)
-void initialize(){
- freeList->tamanho=20000-sizeof(struct block);
- freeList->free=1;
- freeList->next=NULL;
+// inicializando o bloco de memória com tamanho máximo (memoLivre é o bloco principal)
+void  inicializar(){
+    memoLivre->tamanho=20000-sizeof(struct bloco);
+    memoLivre->libera=1;
+    memoLivre->prox=NULL;
 }
 
-//Making way for a new block allocation by splitting a free block i.e freeList using First Fit
-void split(struct block *fitting_slot,size_t tamanho){
- struct block *new=(void*)((void*)fitting_slot+tamanho+sizeof(struct block));
- new->tamanho=(fitting_slot->tamanho)-tamanho-sizeof(struct block);
- new->free=1;
- new->next=fitting_slot->next;
- fitting_slot->tamanho=tamanho;
- fitting_slot->free=0;
- fitting_slot->next=new;
+// Abrindo caminho para uma nova alocação de bloco dividindo um bloco livre, ou seja, memoLivre usando o First Fit
+void quebra(struct bloco *encaixe_de_memo,size_t tamanho){
+    struct bloco *new=(void*)((void*)encaixe_de_memo+tamanho+sizeof(struct bloco));
+    new->tamanho=(encaixe_de_memo->tamanho)-tamanho-sizeof(struct bloco);
+    new->libera=1;
+    new->prox=encaixe_de_memo->prox;
+    encaixe_de_memo->tamanho=tamanho;
+    encaixe_de_memo->libera=0;
+    encaixe_de_memo->prox=new;
 }
 
-//mymallc function
-//basically splits needed memoria from freelist and links nodes (kinda like a linked list)
-void *mymalloc(size_t noOfBytes){
- struct block *curr,*prev;
- void *result;
- if(!(freeList->tamanho)){
-  initialize();
-  printf("memoria initialized\n");
- }
- curr=freeList;
- while((((curr->tamanho)<noOfBytes)||((curr->free)==0))&&(curr->next!=NULL)){
-  prev=curr;
-  curr=curr->next;
-  printf("One block checked\n");
- }
- if((curr->tamanho)==noOfBytes){
-  curr->free=0;
-  result=(void*)(++curr);
-  printf("Exact fitting block allocated\n");
-  return result;
- }
- else if((curr->tamanho)>(noOfBytes+sizeof(struct block))){
-  split(curr,noOfBytes);
-  result=(void*)(++curr);
-  printf("Fitting block allocated with a split\n");
-  return result;
- }
- else{
-  result=NULL;
-  printf("Sorry. No sufficient memoria to allocate\n");
-  return result;
- }
+// basicamente divide a memória necessária da memoLivre e dos nós de links (como uma lista vinculada)
+void *aloca(size_t nDeBytes){
+    struct bloco *atual,*prev;
+    void *resultado;
+    if(!(memoLivre->tamanho)){
+        inicializar();
+        printf("Memória inicializada\n");
+    }
+    atual=memoLivre;
+    while((((atual->tamanho)<nDeBytes)||((atual->libera)==0))&&(atual->prox!=NULL)){
+        prev=atual;
+        atual=atual->prox;
+        printf("Um bloco foi verificado\n");
+    }
+    if((atual->tamanho)==nDeBytes){
+        atual->libera=0;
+        resultado=(void*)(++atual);
+        printf("Bloco de encaixe exato alocado\n");
+        return resultado;
+    }
+    else if((atual->tamanho)>(nDeBytes+sizeof(struct bloco))){
+        quebra(atual,nDeBytes);
+        resultado=(void*)(++atual);
+        printf("Bloco de encaixe alocado com uma divisão\n");
+        return resultado;
+    }
+    else{
+        resultado=NULL;
+        printf("Desculpe. Não há memória suficiente para alocar\n");
+        return resultado;
+    }
 }
 
-//helper function for freeing memoria when deallocting
-void merge(){
- struct block *curr,*prev;
- curr=freeList;
- while((curr->next)!=NULL){
-  if((curr->free) && (curr->next->free)){
-   curr->tamanho+=(curr->next->tamanho)+sizeof(struct block);
-   curr->next=curr->next->next;
-  }
-  prev=curr;
-  curr=curr->next;
- }
+// função auxiliar para liberar memória ao desalocar
+void misturar(){
+    struct bloco *atual,*prev;
+    atual=memoLivre;
+    while((atual->prox)!=NULL){
+        if((atual->libera) && (atual->prox->libera)){
+            atual->tamanho+=(atual->prox->tamanho)+sizeof(struct bloco);
+            atual->prox=atual->prox->prox;
+            }
+        prev=atual;
+        atual=atual->prox;
+    }
 }
 
-//myfree i.e returning memoria to freeList and deleting the block
-void myfree(void* ptr){
- if(((void*)memoria<=ptr)&&(ptr<=(void*)(memoria+20000))){
-  struct block* curr=ptr;
-  --curr;
-  curr->free=1;
-  merge();
- }
- else printf("Please provide a valid pointer allocated by MyMalloc\n");
+//libera, ou seja, retornando memória para memoLivre e excluindo o bloco
+void libera(void* ptr){
+    if(((void*)memoria<=ptr)&&(ptr<=(void*)(memoria+20000))){
+        struct bloco* atual=ptr;
+        --atual;
+        atual->libera=1;
+        misturar();
+    }
+    else printf("Forneça um ponteiro válido alocado por aloca\n");
 }
 
-//TEST
+//TESTE
 int main(){
- 
- int *p=(int)mymalloc(100*sizeof(int));
- char *q=(char)mymalloc(250*sizeof(char));
- int *r=(int)mymalloc(100*sizeof(int));
- myfree(p);
- myfree(p);
- char *w=(char)mymalloc(700);
- myfree(r);
- int *k=(int)mymalloc(500*sizeof(int));
+    int *p=(int)aloca(100*sizeof(int));
+    char *q=(char)aloca(250*sizeof(char));
+    int *r=(int)aloca(100*sizeof(int));
+    libera(p);
+    libera(p);
+    char *w=(char)aloca(700);
+    libera(r);
+    int *k=(int)aloca(500*sizeof(int));
+    libera(q);
 }
